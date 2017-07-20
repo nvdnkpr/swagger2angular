1a3b96dcbb584dd22a0505e7eb47ba6e2677550d
Navid Nikpour <navid@nikpour.com>
2017-07-20 16:15:05 +0200

    Updates package.json and gulpfile, adding new gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
 const del = require('del');
+const gulp = require('gulp');
+const beautify = require('gulp-beautify');
+const debug = require('gulp-debug');
+const jsonTransform = require('gulp-json-transform');
+const ts = require('gulp-typescript');
 
 gulp.task('default', ['clean', 'copyPackageJson', 'copyTemplates', 'copyLib']);
 
-gulp.task('clean', function(){ return del(['dist']); });
+gulp.task('clean', function () {
+  return del(['dist']);
+});
 
-gulp.task('copyTemplates',['clean'], function(){ return gulp.src('src/**/*.hbs').pipe(gulp.dest('dist')); });
+gulp.task('copyTemplates', ['clean'], function () {
+  return gulp.src('src/**/*.hbs').pipe(gulp.dest('dist'));
+});
 
-gulp.task('copyPackageJson', ['clean'], function(){ return gulp.src('src/package.json').pipe(gulp.dest('dist')); });
+gulp.task('copyPackageJson', ['clean'], function () {
+  return gulp.src('package.json')
+    .pipe(jsonTransform((packageJson, file) => {
+        delete packageJson.scripts['build'];
+        delete packageJson['devDependencies'];
 
-gulp.task('copyLib',['clean'], function(){ return gulp.src(['src/**/*.ts', '!src/lib/util/**/*.ts']).pipe(ts({project: 'tsconfig.json'})).pipe(gulp.dest('dist')); });
+        return packageJson;
+      }
+    ))
+    .pipe(beautify({indent_size: 2}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyLib', ['clean'], function () {
+  return gulp.src(['src/**/*.ts', '!src/lib/util/**/*.ts']).pipe(ts({project: 'tsconfig.json'})).pipe(gulp.dest('dist'));
+});
